8d6e6247d93ea3b2a85f7139754fa97def8f79bd
Gilson Sales <gilson.sales21@hotmail.com>
2018-10-21 12:47:44 -0300

    Update parcial
    limpeza de codigo e comentario das funções.

--- app/Http/Controllers/ControladorNoticias.php
+++ app/Http/Controllers/ControladorNoticias.php
@@ -7,31 +7,19 @@ use App\Noticia;
 
 class ControladorNoticias extends Controller
 {
+    //retorno de view basica, requisições json tratadas na propria view.
     public function indexView()
     {
         return view('noticias');
     }
+    /*retorna a view da noticia junto com o id para que possa ser passado
+      para o scrippt json. */  
     public function vermais($id)
     {          
         return view('noticiap')->with('id', $id);   
     }
     
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     
 }
 
